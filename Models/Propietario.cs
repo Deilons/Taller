1128dbcc8f4755f9e136f92bc2c27dc4dfc6f7bf
Deilons <apolo4748@gmail.com>
2024-09-02 07:24:04 -0500

    feat: data annotations

--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Taller.Models;
 
 public class Propietario
-{
+{   
+    [Key]
     public int Id { get; set; }
     public required string Nombre { get; set; }
     public required string Apellido { get; set; }
--- Models/Vehiculos.cs
+++ Models/Vehiculos.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Taller.Models;
 
 public class Vehiculos
-{
+{   
+    [Key]
     public int Id { get; set; }
     public required string Marca { get; set; }
     public required string Modelo { get; set; }
+    public required int PropietarioId { get; set; }
     public required int Año { get; set; }
     public required string Color { get; set; }
     public required string TipoVehiculo { get; set; }
+
+    [ForeignKey("PropietarioId")]
+    public Propietario? Propietario { get; set; }
+
 }
