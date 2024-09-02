43bd00fc93c74a7ece6fc66024d1de517b241915
Deilons <apolo4748@gmail.com>
2024-09-02 07:12:54 -0500

    feat: modelo vehiculo

--- Models/Vehiculos.cs
+++ Models/Vehiculos.cs
@@ -7,5 +7,10 @@ namespace Taller.Models;
 
 public class Vehiculos
 {
-
+    public int Id { get; set; }
+    public required string Marca { get; set; }
+    public required string Modelo { get; set; }
+    public required int Año { get; set; }
+    public required string Color { get; set; }
+    public required string TipoVehiculo { get; set; }
 }
